290a97d4bdcf76ca10693c57a757a74f385513b3
Suika <suikasibyl@gmail.com>
2022-04-21 22:35:39 +0800

    GFX :: Fix Bloom Bug caused by non-clamp sampler

--- Engine/Source/Runtime/RHI/Private/Vendors/Vulkan/Resource/ISamplerVK.cpp
+++ Engine/Source/Runtime/RHI/Private/Vendors/Vulkan/Resource/ISamplerVK.cpp
@@ -16,9 +16,9 @@ namespace SIByL::RHI
 		samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
 		samplerInfo.magFilter = VK_FILTER_LINEAR;
 		samplerInfo.minFilter = VK_FILTER_LINEAR;
-		samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-		samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-		samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+		samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+		samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+		samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
 		samplerInfo.anisotropyEnable = VK_TRUE;
 
 		VkPhysicalDeviceProperties properties{};
